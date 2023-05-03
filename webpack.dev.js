1052bafaa45a2d90049adf0c7209a0bfbbfa11a0
Tania <taryutova@yandex.ru>
2023-05-03 21:06:43 +0300

    deleted vscode

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,7 +10,6 @@ module.exports = merge(common, {
 
   // Spin up a server for quick development
   devServer: {
-    port: 9000,
     historyApiFallback: true,
     open: true,
     compress: true,
